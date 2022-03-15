cfc4a12589e6d114dc4b3b1e1b3d202c853ff1d5
Masaki Waga <masakiwaga@gmail.com>
2022-03-15 18:51:44 +0900

    Made an interface of EquivalenceOracle

---
+++ include/equivalence_oracle.hh
@@ -0,0 +1,25 @@
+/**
+ * @author Masaki Waga
+ * @date 2022/03/15.
+ */
+
+#pragma once
+
+#include <optional>
+
+#include "timed_automaton.hh"
+#include "timed_word.hh"
+
+namespace learnta {
+  /*!
+   * @brief Interface of the equivalence oracle
+   */
+  class EquivalenceOracle {
+  public:
+    virtual ~EquivalenceOracle() = default;
+    /*!
+     * @brief Make an equivalence query
+     */
+    virtual std::optional<TimedWord> findCounterExample(const TimedAutomaton &automaton) = 0;
+  };
+}
