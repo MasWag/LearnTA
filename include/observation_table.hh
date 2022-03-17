68578a69534e5848868745f3e90da49192a5dd36
Masaki Waga <masakiwaga@gmail.com>
2022-03-17 20:16:02 +0900

    Implemented functions to make the observation table closed and consistent

--- include/observation_table.hh
+++ include/observation_table.hh
@@ -13,6 +13,7 @@
 #include "backward_regional_elementary_language.hh"
 #include "symbolic_membership_oracle.hh"
 #include "timed_automaton.hh"
+#include "equivalence.hh"
 
 namespace learnta {
   /*!
@@ -27,11 +28,13 @@ namespace learnta {
     // The indexes of prefixes in P
     std::unordered_set<std::size_t> pIndices;
     // prefixes[first] and prefixes[second.first] are in the same equivalence class witnessed by second.second
-    std::unordered_map<std::size_t, std::pair<std::size_t, std::vector<std::pair<std::size_t, std::size_t>>>> closedRelation;
+    std::unordered_map<std::size_t, std::unordered_map<std::size_t, std::vector<std::pair<std::size_t, std::size_t>>>> closedRelation;
     // The table containing the symbolic membership
     std::vector<std::vector<TimedConditionSet>> table;
     std::unordered_map<std::size_t, std::size_t> continuousSuccessors;
     std::unordered_map<std::pair<std::size_t, Alphabet>, std::size_t> discreteSuccessors;
+    // The pair of prefixes such that we know that they are distinguished
+    std::unordered_set<std::pair<std::size_t, std::size_t>> distinguishedPrefix;
 
     // Fill the observation table
     void refreshTable() {
@@ -52,10 +55,6 @@ namespace learnta {
       // index should not be in P yet
       assert(pIndices.find(index) == pIndices.end());
       pIndices.insert(index);
-      auto it = closedRelation.find(index);
-      if (it != closedRelation.end()) {
-        closedRelation.erase(it);
-      }
       // Add successors to the prefixes
       prefixes.reserve(prefixes.size() + alphabet.size() + 1);
       for (Alphabet c: alphabet) {
@@ -66,6 +65,47 @@ namespace learnta {
       continuousSuccessors.at(index) = prefixes.size();
       // fill the observation table
       refreshTable();
+    }
+
+    bool equivalent(std::size_t i, std::size_t j) {
+      auto renamingRelation = findEquivalentRenaming(this->prefixes.at(i), this->table.at(i),
+                                                     this->prefixes.at(j), this->table.at(j), this->suffixes);
+      if (renamingRelation) {
+        this->closedRelation[i][j] = renamingRelation.value();
+        return true;
+      } else {
+        this->distinguishedPrefix.insert(std::make_pair(i, j));
+        return false;
+      }
+    }
+
+    bool equivalentWithMemo(std::size_t i, std::size_t j) {
+      if (this->distinguishedPrefix.find(std::make_pair(i, j)) != this->distinguishedPrefix.end() ||
+          this->distinguishedPrefix.find(std::make_pair(j, i)) != this->distinguishedPrefix.end()) {
+        // we already know that they are not equivalent
+        return false;
+      }
+      auto it = this->closedRelation.at(i).find(j);
+      if (it != this->closedRelation.at(i).end()) {
+        if (equivalence(this->prefixes.at(i), this->table.at(i),
+                        this->prefixes.at(j), this->table.at(j), this->suffixes, it->second)) {
+          return true;
+        }
+      }
+      return this->equivalent(i, j);
+    }
+
+    [[nodiscard]] bool
+    equivalent(std::size_t i, std::size_t j, const BackwardRegionalElementaryLanguage &newSuffix) const {
+      auto leftRow = this->table.at(i);
+      leftRow.push_back(this->memOracle->query(prefixes.at(i) + newSuffix));
+      auto rightRow = this->table.at(j);
+      rightRow.push_back(this->memOracle->query(prefixes.at(j) + newSuffix));
+      auto newSuffixes = this->suffixes;
+      newSuffixes.push_back(newSuffix);
+
+      return findEquivalentRenaming(this->prefixes.at(i), leftRow, this->prefixes.at(j), rightRow,
+                                    newSuffixes).has_value();
     }
 
     explicit ObservationTable(std::vector<Alphabet> alphabet, std::unique_ptr<SymbolicMembershipOracle> memOracle) :
@@ -91,21 +131,94 @@ namespace learnta {
     /*!
      * @brief Close the observation table
      *
-     * @returns if the observation table is already closed
-     * @todo Implement this
+     * @returns This returns true if the observation table is already closed
      */
     bool close() {
-      abort();
+      for (std::size_t i = 0; i < this->prefixes.size(); i++) {
+        {
+          // Check if this prefix is in P
+          auto it = this->pIndices.find(i);
+          if (it != this->pIndices.end()) {
+            continue;
+          }
+        }
+        auto prefix = this->prefixes.at(i);
+        auto prefixRow = this->table.at(i);
+        bool found = false;
+        {
+          auto it = this->closedRelation.find(i);
+          if (it != closedRelation.end()) {
+            // When we already know that this prefix is equivalent to one of p \in P, we just confirm it.
+            for (auto targetIt = it->second.begin(); targetIt != it->second.end();) {
+              auto renamingRelation = targetIt->second;
+              if (equivalence(prefix, prefixRow,
+                              this->prefixes.at(targetIt->first), this->table.at(targetIt->first), this->suffixes,
+                              renamingRelation)) {
+                found = true;
+                break;
+              } else {
+                targetIt = this->closedRelation.at(i).erase(targetIt);
+              }
+            }
+          }
+        }
+        // We have no idea of the target prefix, and we find an equivalent row
+        if (!found) {
+          found = std::any_of(this->pIndices.begin(), this->pIndices.end(), [&](const auto j) {
+            return equivalent(i, j);
+          });
+        }
+        if (!found) {
+          // The observation table is not closed
+          this->moveToP(i);
+          return false;
+        }
+      }
+
+      return true;
     }
 
     /*!
      * @brief Make the observation table consistent
      *
-     * @returns if the observation table is already consistent
-     * @todo Implement this
+     * @returns This return true if the observation table is already consistent
      */
     bool consistent() {
-      abort();
+      for (const auto i: pIndices) {
+        for (const auto j: pIndices) {
+          bool eq = false;
+          if (i <= j) {
+            continue;
+          }
+          if (this->equivalentWithMemo(i, j)) {
+            // confirm the consistency
+            for (const auto action: this->alphabet) {
+              if (!this->equivalentWithMemo(this->discreteSuccessors.at({i, action}),
+                                            this->discreteSuccessors.at({j, action}))) {
+                // The observation table is inconsistent due to a discrete successor
+                auto it = std::find(suffixes.begin(), suffixes.end(), [&](const auto &suffix) {
+                  return !equivalent(i, j, suffix.predecessor(action));
+                });
+                // we assume that we have such a suffix
+                suffixes.push_back(it->predecessor(action));
+
+                return false;
+              }
+            }
+            if (!this->equivalentWithMemo(this->continuousSuccessors.at(i), this->continuousSuccessors.at(j))) {
+              // The observation table is inconsistent due to a continuous successor
+              auto it = std::find(suffixes.begin(), suffixes.end(), [&](const auto &suffix) {
+                return !equivalent(i, j, suffix.predecessor());
+              });
+              // we assume that we have such a suffix
+              suffixes.push_back(it->predecessor());
+
+              return false;
+            }
+          }
+        }
+      }
+      return true;
     }
 
     /*!
@@ -144,6 +257,7 @@ namespace learnta {
     /*!
      * @brief Construct a hypothesis DTA from the current timed observation table
      *
+     * @pre The observation table is closed, consistent, and exterior-consistent.
      * @todo not implemented
      */
     TimedAutomaton generateHypothesis();
