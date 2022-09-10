880c804ed00c85df86f22c912a3034dc7b80eca6
Masaki Waga <masakiwaga@gmail.com>
2022-09-10 09:46:09 +0900

    TimedAutomatonRunner supports empty TA

--- include/timed_automaton_runner.hh
+++ include/timed_automaton_runner.hh
@@ -21,31 +21,42 @@ namespace learnta {
   class TimedAutomatonRunner : public SUL {
   private:
     TimedAutomaton automaton;
-    TAState* state;
+    TAState *state;
     std::vector<double> clockValuation;
     std::size_t numQueries;
+    const bool isEmpty = false;
   public:
-    explicit TimedAutomatonRunner(TimedAutomaton automaton) : automaton(std::move(automaton)) {
-      assert(this->automaton.initialStates.size() == 1);
-      this->state = this->automaton.initialStates.at(0).get();
-      this->clockValuation.resize(this->automaton.maxConstraints.size());
+    explicit TimedAutomatonRunner(TimedAutomaton automaton) : automaton(std::move(automaton)),
+                                                              isEmpty(this->automaton.states.empty()) {
+      if (!isEmpty) {
+        assert(this->automaton.initialStates.size() == 1);
+        this->state = this->automaton.initialStates.at(0).get();
+        this->clockValuation.resize(this->automaton.maxConstraints.size());
+      }
       numQueries = 0;
     }
 
     //! @brief Reset the configuration
     void pre() override {
-      // We assume that the given timed automaton is deterministic
-      assert(this->clockValuation.size() == this->automaton.maxConstraints.size());
-      this->state = this->automaton.initialStates.at(0).get();
-      std::fill(this->clockValuation.begin(), this->clockValuation.end(), 0);
+      if (!isEmpty) {
+        // We assume that the given timed automaton is deterministic
+        assert(this->clockValuation.size() == this->automaton.maxConstraints.size());
+        this->state = this->automaton.initialStates.at(0).get();
+        std::fill(this->clockValuation.begin(), this->clockValuation.end(), 0);
+      }
       numQueries++;
     }
 
     void post() override {
-      assert(this->clockValuation.size() == this->automaton.maxConstraints.size());
+      if (!isEmpty) {
+        assert(this->clockValuation.size() == this->automaton.maxConstraints.size());
+      }
     }
 
     bool step(char action) override {
+      if (isEmpty) {
+        return false;
+      }
       assert(this->clockValuation.size() == this->automaton.maxConstraints.size());
       // Skip the operation if we are at the sink state
       if (this->state == nullptr) {
@@ -53,12 +64,12 @@ namespace learnta {
       }
       for (const TATransition &transition: this->state->next[action]) {
         // Check if the guard is satisfied
-        if (std::all_of(transition.guard.begin(), transition.guard.end(), [&] (const Constraint &guard) {
+        if (std::all_of(transition.guard.begin(), transition.guard.end(), [&](const Constraint &guard) {
           return guard.satisfy(this->clockValuation.at(guard.x));
         })) {
           // Reset the clock variables
           const auto oldValuation = this->clockValuation;
-          for (const auto& [resetVariable, targetVariable]: transition.resetVars) {
+          for (const auto &[resetVariable, targetVariable]: transition.resetVars) {
             if (targetVariable) {
               this->clockValuation.at(resetVariable) = oldValuation.at(*targetVariable);
             } else {
@@ -77,17 +88,22 @@ namespace learnta {
     }
 
     bool step(double duration) override {
+      if (isEmpty) {
+        return false;
+      }
       assert(this->clockValuation.size() == this->automaton.maxConstraints.size());
       // Skip the operation if we are at the sink state
       if (this->state == nullptr) {
         return false;
       }
-      std::transform(this->clockValuation.begin(), this->clockValuation.end(), this->clockValuation.begin(), [&] (double value) {
-        return value + duration;
-      });
+      std::transform(this->clockValuation.begin(), this->clockValuation.end(), this->clockValuation.begin(),
+                     [&](double value) {
+                       return value + duration;
+                     });
 
       return this->state->isMatch;
     }
+
     [[nodiscard]] std::size_t count() const override {
       return numQueries;
     }
