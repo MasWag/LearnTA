566828dfb8d654742dc4a50cffe165138d24ae93
Masaki Waga <masakiwaga@gmail.com>
2023-01-12 21:09:33 +0900

    added a warning message for merging of transitions with different targets

--- src/timed_automaton.cc
+++ src/timed_automaton.cc
@@ -139,11 +139,9 @@ namespace learnta {
             // However, it is not straightforward to check the equivalence.
             // So, we tentatively weaken the requirement
             assert(it2->target->isMatch == it3->target->isMatch);
-#ifdef DEBUG
             if (it2->target != it3->target) {
-              BOOST_LOG_TRIVIAL(debug) << "merge " << it2->target << " and " << it3->target;
+              BOOST_LOG_TRIVIAL(warning) << "We merge transitions with different targets. This is unstable";
             }
-#endif
             // Use the reset and target causing more imprecise clocks
             //if (it2->resetVars.size() < it3->resetVars.size()) {
             if (TATransition::impreciseConstantAssignSize(it2->resetVars) <
